dfa2d5e0d7f829ec1714705c91bf62cc87d761f8
Angel Hernandez <angel@magic.link>
2025-04-16 10:23:43 -0400

    feat: add context providers

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { UserProvider } from "./context/UserContext";
+import { LoginProvider } from "./context/LoginContext";
+import { MFASettingsProvider } from "./context/MFASettingsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <App />
+      <LoginProvider>
+        <MFASettingsProvider>
+          <App />
+        </MFASettingsProvider>
+      </LoginProvider>
     </UserProvider>
   </React.StrictMode>
 );
